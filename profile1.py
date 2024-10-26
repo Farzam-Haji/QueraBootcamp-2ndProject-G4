2204697a65332d8893b1fb6dc347d399ec3e93bd
kimia <lianaaa2828@gmail.com>
2024-10-26 19:00:33 +0330

    connected to db

--- profile1.py
+++ profile1.py
@@ -1,43 +1,65 @@
-from flask import Flask, render_template, redirect, url_for, request, flash , g, Blueprint, session
+from flask import Flask, render_template, redirect, url_for, request, flash , g, Blueprint, session , abort
 import sqlite3
 from functools import wraps
 
 profile1 = Blueprint ("profile1", __name__, template_folder="templates")
 
-user_data = {
-    'name': 'username',
-    'last_name': 'user lastname',
-    'email': 'user@email.com',
-    'age': 30,
-    'username': 'user123',
-    'password': '1234',
-    'marks': [10, 8 , 7 ,5, 9]
-}
+def get_db():
+    if 'db' not in g:
+        g.db = sqlite3.connect('quiz.db')
+        g.db.row_factory = sqlite3.Row
+    return g.db
 
-@profile1.route('/profile')
-def profile():
-    return render_template('profile.html', user=user_data)
 
-@profile1.route('/edit_profile', methods=['GET', 'POST'])
-def edit_profile():
-    if request.method == 'POST':
-        user_data['name'] = request.form['name']
-        user_data['last_name'] = request.form['last_name']
-        user_data['email'] = request.form['email']
-        user_data['age'] = request.form['age']
-        print(user_data['password'])
+@profile1.route('/profile/<username>')
+def profile(username):
+    conn = get_db()
+    user = conn.execute('SELECT * FROM users WHERE username = ?', (username,)).fetchone()
+    conn.close()
 
-        password = request.form.get('password')
-        if password:
-            user_data['password'] = password
-            print(user_data['password']) #something
-            pass
+    if user is None:
+        abort(404)  #404 error
         
-        flash('Profile updated successfully !', 'success')
-        return redirect(url_for('profile1.profile'))
+    return render_template('profile.html', user=user)
 
-    return render_template('edit_profile.html', user=user_data)
 
-@profile1.route('/quiz_marks')
-def quiz_marks():
-    return render_template('quiz_marks.html', marks=user_data['marks'])
+@profile1.route('/edit_profile/<username>', methods=['GET', 'POST'])
+def edit_profile(username):
+    conn = get_db()
+
+    if request.method == 'POST':
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        email = request.form['email']
+        age = request.form['age']
+        password = request.form['password']
+
+        conn.execute('UPDATE users SET first_name = ?, last_name = ?, email = ?, age = ?, password = ? WHERE username = ?',
+                     (first_name, last_name, email, age, password, username))
+        conn.commit()
+        conn.close()
+        return redirect(url_for('profile1.profile', username=username))
+
+    user = conn.execute('SELECT * FROM users WHERE username = ?', (username,)).fetchone()
+    conn.close()
+
+    if user is None:
+        abort(404)
+
+    return render_template('edit_profile.html', user=user)
+
+
+@profile1.route('/quiz_marks/<username>')
+def quiz_marks(username):
+    conn = get_db()
+    #fetch
+    user = conn.execute('SELECT * FROM users WHERE Username = ?', (username,)).fetchone()
+    conn.close()
+
+    if user is None:
+        abort(404)
+
+    #quiz list
+    quiz_marks = user['quiz_results'].split(',') if user['quiz_results'] else []
+
+    return render_template('quiz_marks.html', username=username, marks=quiz_marks)
